5c644b3fe136613581100460c83ec29808d42a4f
ledhieus <ledan0602@gmail.com>
2024-12-20 09:51:31 +0700

    update

--- src/pages/Home/QuickItem.js
+++ src/pages/Home/QuickItem.js
@@ -5,9 +5,10 @@ import { useSelector } from "react-redux"
 import { useEffect, useState } from 'react';
 import { getItemById } from "../../services/product"
 import { Col, Row } from "antd";
-import { quantityCart } from "../../actions/quantityCart";
+import { quantityCart, updateQuantityCart } from "../../actions/quantityCart";
 import { addToCart, getCart } from "../../helper/localStorage";
 import { notification } from 'antd';
+import { sumPrice } from "../../helper/feature";
 
 function QuickItem(props) {
     const idProduct = useSelector(state => state.infoQuickItemReducer)
@@ -16,7 +17,8 @@ function QuickItem(props) {
     const [quantity, setQuantity] = useState(1)
     const [infoItem, setInfoItem] = useState([])
     const dispatch = useDispatch()
-    const cartItem = getCart().length
+
+    const cartItem = getCart()
 
     const handleClose = () => {
         dispatch(openQuickItem(false))
@@ -30,7 +32,7 @@ function QuickItem(props) {
     }, [idProduct])
     const handleAddtoCart = () => {
         let infoProduct;
-        if(infoItem[0].size){
+        if (infoItem[0].size) {
             infoProduct = {
                 productId: infoItem[0].id,
                 productTitle: infoItem[0].title,
@@ -40,7 +42,7 @@ function QuickItem(props) {
                 quantity: quantity
             }
         }
-        else{
+        else {
             infoProduct = {
                 productId: infoItem[0].id,
                 productTitle: infoItem[0].title,
@@ -49,8 +51,10 @@ function QuickItem(props) {
                 quantity: quantity
             }
         }
-        
-        dispatch(quantityCart(cartItem + 1))
+        const total = sumPrice(cartItem) + infoProduct.price * infoProduct.quantity
+        dispatch(updateQuantityCart(total))
+
+        dispatch(quantityCart(cartItem.length + 1))
         dispatch(openQuickItem(false))
         addToCart(infoProduct)
 
@@ -58,7 +62,7 @@ function QuickItem(props) {
             message: 'Thêm vào giỏ hàng thành công thành công',
             description: "Hãy vào giỏ hàng để đặt hàng ngay",
             placement: 'topRight',
-          });
+        });
     }
 
     const handleSize = (e) => {
@@ -83,45 +87,49 @@ function QuickItem(props) {
     }
     return (
         <>
-            {infoItem.length > 0 && <Row gutter={[30, 30]} className={`quick-wrap ${props.isOpen ? "active" : ""}`}>
-                <Col span={8} className="image-wrap">
-                    <div className="image-item">
-                        {infoItem.length > 0 && <img src={infoItem[0].images[0]} />}
-                    </div>
-                </Col>
-                <Col span={16} className="info-wrap">
-                    <h2 className="title">{infoItem[0].title}</h2>
-                    <div className="nameCode">
-                        <p className="name">Thương hiệu: <span>MIXI</span></p>
-                        <p className="code">Mã sản phẩm: <span>{infoItem[0].code}</span></p>
-                    </div>
-                    <span className="price">{infoItem[0].price}.000đ</span>
-                    <p className="size">Kích thước:</p>
-                    {infoItem[0].size && <div className="size-wrap">
-                        {infoItem[0].size.map((item, index) => (
-                            <button onClick={handleSize} name={item}
-                            className={`size-bt ${size === item ? "active" : ""}`} key={index}>{item}</button>
-                        ))}
-                    </div>}
-                    <div className="promotion">
-                        <ul>
-                            <li>Chuyển khoản với đơn hàng từ 500k trở lên (Bắt buộc)</li>
-                            <li>Đồng giá ship toàn quốc 30k</li>
-                            <li>Hỗ trợ trả lời thắc mắc qua fanpage chính thức</li>
-                            <li>Khuyến mãi trực tiếp trên giá sản phẩm</li>
-                            <li>Đổi trả nếu sản phẩm lỗi bất kì</li>
-                        </ul>
-                    </div>
-                    <div className="detail-product__input">
-                        <div className="input">
-                            <button className="input-bt subtract" onClick={handleSubtract}>-</button>
-                            <input value={quantity} type="text" onChange={handleChange} />
-                            <button className="input-bt plus" onClick={handlePlus}>+</button>
-                        </div>
-                        <button onClick={handleAddtoCart}>THÊM VÀO GIỎ HÀNG</button>
-                    </div>
-                </Col>
-            </Row>}
+            {props.isOpen && (
+                <>
+                    {infoItem.length > 0 && <Row gutter={[30, 30]} className="quick-wrap active">
+                        <Col span={8} className="image-wrap">
+                            <div className="image-item">
+                                {infoItem.length > 0 && <img src={infoItem[0].images[0]} />}
+                            </div>
+                        </Col>
+                        <Col span={16} className="info-wrap">
+                            <h2 className="title">{infoItem[0].title}</h2>
+                            <div className="nameCode">
+                                <p className="name">Thương hiệu: <span>MIXI</span></p>
+                                <p className="code">Mã sản phẩm: <span>{infoItem[0].code}</span></p>
+                            </div>
+                            <span className="price">{infoItem[0].price}.000đ</span>
+                            <p className="size">Kích thước:</p>
+                            {infoItem[0].size && <div className="size-wrap">
+                                {infoItem[0].size.map((item, index) => (
+                                    <button onClick={handleSize} name={item}
+                                        className={`size-bt ${size === item ? "active" : ""}`} key={index}>{item}</button>
+                                ))}
+                            </div>}
+                            <div className="promotion">
+                                <ul>
+                                    <li>Chuyển khoản với đơn hàng từ 500k trở lên (Bắt buộc)</li>
+                                    <li>Đồng giá ship toàn quốc 30k</li>
+                                    <li>Hỗ trợ trả lời thắc mắc qua fanpage chính thức</li>
+                                    <li>Khuyến mãi trực tiếp trên giá sản phẩm</li>
+                                    <li>Đổi trả nếu sản phẩm lỗi bất kì</li>
+                                </ul>
+                            </div>
+                            <div className="detail-product__input">
+                                <div className="input">
+                                    <button className="input-bt subtract" onClick={handleSubtract}>-</button>
+                                    <input value={quantity} type="text" onChange={handleChange} />
+                                    <button className="input-bt plus" onClick={handlePlus}>+</button>
+                                </div>
+                                <button onClick={handleAddtoCart}>THÊM VÀO GIỎ HÀNG</button>
+                            </div>
+                        </Col>
+                    </Row>}
+                </>
+            )}
             <div className={`overlay-quick ${props.isOpen ? "visible" : ""}`}
                 onClick={handleClose}
             ></div>
